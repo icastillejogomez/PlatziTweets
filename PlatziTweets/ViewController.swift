d67a4d3c656c864b4195261da2380fab2835cebe
Ignacio Castillejo <i.castillejogomez@gmail.com>
2020-06-05 20:24:23 +0200

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by Ignacio F. Castillejo Gómez on 05/06/2020.
+//  Copyright © 2020 Ignacio F. Castillejo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
